4bf36c810003dc5ead6abbe7a8b3d27f5709a5f3
anthonySchafrik <anthonySchafrik@gmail.com>
2018-10-23 23:34:40 -0400

    WIll a way to input todos

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => (
   <div id="header">
     <h1>Notes and todos.</h1>
-    <div id="toggleButton" />
+    <div>
+      <input placeholder="Enter Todo" />
+    </div>
   </div>
 );
 
